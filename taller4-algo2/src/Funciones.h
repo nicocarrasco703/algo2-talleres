85fc546531767403b081e0f803188ba46ab6e396
Nicolas Carrasco <ncarrasco@dc.uba.ar>
2022-09-16 11:08:14 -0300

    Extraidos Meses.h y las Funciones.cpp

---
+++ taller4-algo2/src/Funciones.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicolás Carrasco on 16/09/2022.
+//
+
+#include "Meses.h"
+
+#ifndef SOLUCION_FUNCIONES_H
+#define SOLUCION_FUNCIONES_H
+
+bool esBisiesto(Anio anio);
+
+int diasEnMes(int anio, int mes);
+
+#endif //SOLUCION_FUNCIONES_H
