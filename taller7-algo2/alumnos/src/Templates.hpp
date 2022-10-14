610f647f26f7f20e43857fead8ce00e06cfcb0a8
nicocarrasco703 <ncarrasco@dc.uba.ar>
2022-10-14 10:08:19 -0300

    Agregado Taller 7

---
+++ taller7-algo2/alumnos/src/Templates.hpp
@@ -0,0 +1,14 @@
+//
+// Created by clinux01 on 14/10/22.
+//
+
+#ifndef ALGO2_TALLERES_TEMPLATES_HPP
+#define ALGO2_TALLERES_TEMPLATES_HPP
+
+
+template<class T>
+class cuadrado{
+
+};
+
+#endif //ALGO2_TALLERES_TEMPLATES_HPP
